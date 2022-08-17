40e5ad82c2af326469e57a6decbfa80f0d22d62e
Raijin Albino <ralbino@hawk.iit.edu>
2022-08-17 11:59:54 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <list>
+
+using namespace std;
+
+list<string> Cart;
+string CartList = "";
+
+class ShoppingCart {
+public:
+    string ItemOne, ItemTwo, ItemThree;
+
+    virtual void addToCart(string item) {
+        ItemOne = item;
+        Cart.emplace_front(ItemOne);
+        cout << ItemOne << " has been added to cart." << endl;
+    }
+};
+
+class ShoppingCartExtra:public ShoppingCart {
+public:
+    using ShoppingCart::addToCart;
+
+    void addToCart(string item, string item2) {
+        ItemOne = item, ItemTwo = item2;
+        Cart.emplace_front(ItemOne);
+        Cart.emplace_front(ItemTwo);
+        cout << ItemOne << " and " << ItemTwo << " has been added to cart." << endl;
+    }
+
+    void addToCart(string item, string item2, string item3) {
+        ItemOne = item, ItemTwo = item2, ItemThree = item3;
+        Cart.emplace_front(ItemOne);
+        Cart.emplace_front(ItemTwo);
+        Cart.emplace_front(ItemThree);
+        cout << ItemOne << ", " << ItemTwo << ", and " << ItemThree << " has been added to cart." << endl;
+    }
+
+    void viewCart() {
+        cout << "The cart currently has: " << endl;
+        for(auto& it: Cart) {
+            cout << it << " " << endl;
+        }
+    }
+    
+    void exportToFile() {
+        
+        for(auto& it: Cart) {
+            CartList = CartList + it + "\n";
+        }
+        
+        ofstream CartFile;
+        CartFile.open("cartlist.txt");
+        CartFile << CartList;
+        CartFile.close();
+    }
+};
+
+
+int main() {    // test class
+
+    ShoppingCart test;
+    ShoppingCartExtra test1;
+    test.addToCart("CD");
+    //test.addToCart("0");       // can submit unwanted characters, numbers, etc
+    test1.addToCart("Book", "Magazine");
+    test1.addToCart("Pen", "Paper", "Pencil");
+    test1.viewCart();
+    test1.exportToFile();
+
+    return 0;
+}
